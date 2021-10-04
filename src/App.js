8eb6884efb253d217ef0d93a61506897d879341c
Jose De La Cruz <jose.delcruz0490@icloud.com>
2021-10-04 14:42:38 -0400

    Added the new state variables to take in both age and name content

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { collection, getDocs } from '@firebase/firestore';
 
 function App() {
 	const [users, setUsers] = useState([]);
+	const [newName, setNewName] = useState('');
+	const [newAge, setNewAge] = useState(0);
 	const userCollectionRef = collection(db, 'users');
 
 	const createUser = async () => {};
@@ -12,8 +14,7 @@ function App() {
 	useEffect(() => {
 		const getUsers = async () => {
 			const data = await getDocs(userCollectionRef);
-			console.log(data);
-			// setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+			setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
 		};
 
 		getUsers();
@@ -21,9 +22,19 @@ function App() {
 
 	return (
 		<div className='App'>
+			<input
+				placeholder='Name...'
+				type='text'
+				onChange={(event) => setNewName(event.target.value)}
+			/>
+			<input
+				placeholder='Age...'
+				type='text'
+				onChange={(event) => setNewAge(event.target.value)}
+			/>
 			<button onClick={createUser}>Create User</button>
 			{users.map((user) => (
-				<div>
+				<div key={user.id}>
 					<h1>Name: {user.name}</h1>
 					<h1>Age: {user.age}</h1>
 				</div>
