7e929397f31f9b47271d22c621958b2da420d79b
Jose De La Cruz <jose.delcruz0490@icloud.com>
2021-10-04 14:38:54 -0400

    Added functionality to get data from firebase collection

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import db from './firebase-config';
+import { collection, getDocs } from '@firebase/firestore';
 
 function App() {
-	return <div className='App'></div>;
+	const [users, setUsers] = useState([]);
+	const userCollectionRef = collection(db, 'users');
+
+	const createUser = async () => {};
+
+	useEffect(() => {
+		const getUsers = async () => {
+			const data = await getDocs(userCollectionRef);
+			console.log(data);
+			// setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+		};
+
+		getUsers();
+	});
+
+	return (
+		<div className='App'>
+			<button onClick={createUser}>Create User</button>
+			{users.map((user) => (
+				<div>
+					<h1>Name: {user.name}</h1>
+					<h1>Age: {user.age}</h1>
+				</div>
+			))}
+		</div>
+	);
 }
 
 export default App;
